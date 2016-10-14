062f588361fb076b1887ca02981d4b8e69e2c0e9
fuyongde <fuyongde@foxmail.com>
2016-10-14 18:40:56 +0800

    update

--- black/src/main/java/com/jason/Application.java
+++ black/src/main/java/com/jason/Application.java
@@ -2,11 +2,13 @@ package com.jason;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
 @EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws Exception {
