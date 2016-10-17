dfbb911ef0e81940b369d44ce861ca2011b18fe6
fuyongde <fuyongde@foxmail.com>
2016-10-17 10:49:26 +0800

    使用@SpringBootApplication注解代替@Configuration，@EnableAutoConfiguration，@ComponentScan

--- black/src/main/java/com/jason/Application.java
+++ black/src/main/java/com/jason/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
