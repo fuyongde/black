989865a09b9d21017e1d5293577d0ec1957d64ec
fuyongde <fuyongde@foxmail.com>
2016-10-19 14:27:17 +0800

    update md file.

--- black/src/main/java/com/jason/repository/RegionDao.java
+++ black/src/main/java/com/jason/repository/RegionDao.java
@@ -4,9 +4,6 @@ import com.jason.entity.Region;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-/**
- * Created by fuyongde on 2016/10/18.
- */
 public interface RegionDao extends PagingAndSortingRepository<Region, Integer>, JpaSpecificationExecutor<Region> {
 
 }
