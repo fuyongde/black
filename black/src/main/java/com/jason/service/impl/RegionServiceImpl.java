0feb65239f3d983e11ba06e7650538d99162d09d
fuyongde <fuyongde@foxmail.com>
2016-11-12 17:49:23 +0800

    update

--- black/src/main/java/com/jason/service/impl/RegionServiceImpl.java
+++ black/src/main/java/com/jason/service/impl/RegionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jason.service.impl;
 
 import com.jason.entity.Region;
+import com.jason.repository.jdbc.RegionJdbcDao;
 import com.jason.repository.jpa.RegionDao;
 import com.jason.service.RegionService;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,9 @@ public class RegionServiceImpl implements RegionService {
 
     @Resource
     private RegionDao regionDao;
+
+    @Resource
+    private RegionJdbcDao regionJdbcDao;
 
     @Override
     public Region getById(Integer id) {
