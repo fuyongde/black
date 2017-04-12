824f5b62d6faa5d77b0a713546b855261874d0fd
fuyongde <fuyongde@foxmail.com>
2017-04-12 16:29:37 +0800

    update

--- black/src/main/java/com/jason/black/handler/WebLogAspect.java
+++ black/src/main/java/com/jason/black/handler/WebLogAspect.java
@@ -36,18 +36,20 @@ public class WebLogAspect {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
         // 记录下请求内容
-        logger.info("URL : " + request.getRequestURL().toString());
-        logger.info("HTTP_METHOD : " + request.getMethod());
-        logger.info("IP : " + request.getRemoteAddr());
-        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+        logger.info("=============================start====================================");
+        logger.info("URL : {}", request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : {}", request.getMethod());
+        logger.info("IP : {}", request.getRemoteAddr());
+        logger.info("CLASS_METHOD : {}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : {}", Arrays.toString(joinPoint.getArgs()));
 
     }
 
     @AfterReturning(returning = "ret", pointcut = "webLog()")
     public void doAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
-        logger.info("RESPONSE : " + ret);
-        logger.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+        logger.info("RESPONSE : {}", ret);
+        logger.info("SPEND TIME : {}ms", (System.currentTimeMillis() - startTime.get()));
+        logger.info("=============================end======================================");
     }
 }
