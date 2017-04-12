6b00331784ead42a845f43bca7ab2c35c7a3315e
fuyongde <fuyongde@foxmail.com>
2017-04-12 15:46:49 +0800

    update

--- black/src/main/java/com/jason/black/handler/WebLogAspect.java
+++ black/src/main/java/com/jason/black/handler/WebLogAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -17,6 +18,7 @@ import java.util.Arrays;
  * Created by fuyongde on 2017/4/12.
  */
 @Aspect
+@Component
 public class WebLogAspect {
 
     private static Logger logger = LoggerFactory.getLogger(WebLogAspect.class);
--- black/src/main/java/com/jason/black/web/IndexController.java
+++ black/src/main/java/com/jason/black/web/IndexController.java
@@ -1,13 +1,19 @@
 package com.jason.black.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/index")
 public class IndexController {
+
+    private static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping
     String index() {
+        logger.info("Hello, Spring Boot!");
         return "Hello, Spring Boot!";
     }
 }
