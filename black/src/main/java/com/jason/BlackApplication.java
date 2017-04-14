089cf2f9e5ef47e3129ace5cfd121fe73446f6fe
fuyongde <fuyongde@foxmail.com>
2017-04-14 18:49:25 +0800

    update

--- black/src/main/java/com/jason/BlackApplication.java
+++ black/src/main/java/com/jason/BlackApplication.java
@@ -1,14 +1,7 @@
 package com.jason;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.text.SimpleDateFormat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
@@ -19,16 +12,4 @@ public class BlackApplication {
         SpringApplication.run(BlackApplication.class, args);
     }
 
-    @Bean
-    public HttpMessageConverters gsonHttpMessageConverters() {
-        ObjectMapper mapper = new ObjectMapper();
-        // 设置输出时包含属性的风格
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        // 设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
-
-        return new HttpMessageConverters(converter);
-    }
 }
